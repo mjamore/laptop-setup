2fd0f52c9904468ac89cad6ebb01c2f558426546
Michael Amore <michaelamore@Michaels-MacBook-Pro.local>
2021-12-31 14:11:20 -0800

    Inital commit.

---
+++ README.md
@@ -0,0 +1,56 @@
+# Personal Laptop Setup Script
+This repo serves 2 primary purposes:
+  - Document all of the user preferences, configurations, applications, etc. that I like and want setup on any new machine I use
+  - Provide a script that I can run when I get a new machine to automate as much of the setup process as possible.
+
+
+## Steps to Follow:
+1. Setup System Preferences (Or use Migration Assistant app)
+2. Customize Finder Preferences
+3. Install the following from the Mac App Store
+  - Xcode
+  - Medis
+4. Run setup-script.sh
+5. Add/remove apps from dock
+6. Add VSCode extensions
+  - 
+
+
+## Applications
+### Editor/IDE:
+  - VSCode
+
+### Browsers:
+  - Chrome
+  - Firefox
+
+## Chat
+  - Slack
+  - Discord
+
+## Audio
+  - Audacity
+
+### Development
+  - Xcode command line tools
+  - Medis (Redis client)
+  - Homebrew
+  - git
+  - Docker
+  - AWS CLI
+  - MySQL
+  - Redis
+  - Postman
+  - Nodejs
+  - npm:
+    - nvm
+  - Flutter
+  - React Native CLI
+  - Wireshark
+  - iTerm2
+  - CopyClip
+
+
+## Configuration
+  - Dotfiles (need to create dotfiles repo)
+  - Github ssh key
---
+++ setup-script.sh
@@ -0,0 +1,71 @@
+## Agree to xcode license
+sudo xcodebuild -license accept
+
+## Install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+## Install Chrome
+brew install --cask google-chrome
+
+## Install Firefox
+brew install --cask firefox
+
+## Install VSCode
+brew install --cask visual-studio-code
+
+## Install Slack
+brew install --cask slack
+
+## Install Discord
+brew install --cask discord
+
+## Install Git
+brew install git
+
+## Install Docker
+brew install docker
+brew install --cask docker
+
+## AWS CLI
+brew install awscli
+
+## Install MySQL
+brew install mysql
+brew install --cask mysqlworkbench
+
+## Install Redis
+brew install redis
+
+## Install Postman
+brew install --cask postman
+
+## Install Nodejs
+brew install node
+
+## Install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+## Install Flutter
+brew install --cask flutter
+
+## Install React Native CLI
+brew install react-native-cli
+
+## Install Wireshark
+brew install --cask wireshark
+
+## Install iTerm2
+brew install --cask iterm2
+
+## Install Audacity
+brew install --cask audacity
+
+## Install CopyClip
+brew install --cask copyclip
+
+
+
+
+## Show hidden files
+defaults write com.apple.Finder AppleShowAllFiles true
+killall Finder
